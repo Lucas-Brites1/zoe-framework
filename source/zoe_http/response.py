3889031d12526998ef167a902bb3fd88339db403
Lucas Silva Brites <lucasbrites076@gmail.com>
2026-02-25 19:07:48 -0300

    feat: add recursive auto-serialization for generic objects, dicts and lists in Response

--- source/zoe_http/response.py
+++ source/zoe_http/response.py
@@ -18,7 +18,7 @@ class Response:
 
     def _build(self, *headers: dict[str, Any]) -> bytes:
         response_message = self.__status_line()
-        
+
         body_data = None
         if self.__body is not None:
             body_data = self.__serialize(self.__body)
@@ -32,15 +32,25 @@ class Response:
 
         response_message += "\r\n"
         if body_data is not None:
-            response_message += body_json
+            response_message += body_json # type: ignore
 
         return response_message.encode("utf-8")
 
     def __serialize(self: "Response", obj: Any) -> Any:
-        if hasattr(obj, 'to_dict'):
+        if hasattr(obj, 'to_dict') and callable(obj.to_dict):
             return self.__serialize(obj=obj.to_dict())
+
         elif isinstance(obj, dict):
             return {k: self.__serialize(v) for k,v in obj.items()}
+
         elif isinstance(obj, list):
             return [self.__serialize(i) for i in obj]
-        return obj
+
+        elif hasattr(obj, '__dict__') and not isinstance(obj, type):
+            return self.__serialize(vars(obj))
+
+        elif isinstance(obj, (str, int, float, bool)) or obj is None:
+            return obj
+
+        else:
+            return str(obj)
