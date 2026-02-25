a14de1c3dc009cdf41b68ba6677494c66f3802c2
Lucas Silva Brites <lucasbrites076@gmail.com>
2026-02-25 13:52:09 -0300

    chore: update main to reflect latest framework features and middleware configuration

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
-from source.zoe_net.server import Server
-from source.zoe_application.application import Zoe
-from source.zoe_router.router import Route, Router, Handler
-from source.zoe_http import request, response,code
-from source.zoe_middlewares.logger import Logger
-from source.zoe_middlewares.limiter import Limiter
-
-from source.zoe_schema.model_schema import Model
-from source.zoe_schema.field_schema import Field
-from source.zoe_schema.schema_validators.length import Length
-from source.zoe_schema.schema_validators.not_null import NotNull
-from source.zoe_schema.schema_validators.range import Range
-from source.zoe_schema.schema_validators.pattern import Pattern
-from source.zoe_schema.schema_validators.email import Email
+from zoe_schema.model_schema import Model
+from zoe_schema.field_schema import Field
+from zoe_schema.schema_validators.length import Length
+from zoe_schema.schema_validators.not_null import NotNull
+from zoe_schema.schema_validators.range import Range
+from zoe_schema.schema_validators.pattern import Pattern
+from zoe_schema.schema_validators.email import Email
+from zoe_application.application import Zoe
+from zoe_router.router import Route, Router, Handler
+from zoe_http import request, response, code
+from zoe_middlewares.logger import Logger
+from zoe_middlewares.limiter import Limiter
+from zoe_net.server import Server
 
 class Pessoa(Model):
-    nome: str = Field(NotNull(), Length(max=10))
+    nome: str = Field(NotNull(), Length(max=10, min=5))
     idade: int = Field(NotNull(), Range(min=0, max=130))
     email: str = Field(NotNull(), Email())
 
 class HelloHandler(Handler):
     def __call__(self: "HelloHandler", request: request.Request) -> response.Response:
-        print(request.query_params.message)
         return response.Response(http_status_code=code.HttpCode.OK)
 
 class PayloadHandler(Handler):
@@ -37,7 +35,7 @@ if __name__ == "__main__":
       .POST(endpoint="/user/{user_id}", handler=PayloadHandler())\
       .GET(endpoint="/hello", handler=HelloHandler())
 
-    app.use(router_test).use(Logger()).use(Limiter())
+    app.use(router_test).use(Logger(application_name="Project", verbose=False)).use(Limiter())
 
     server: Server = Server(application=app)
     server.run()
