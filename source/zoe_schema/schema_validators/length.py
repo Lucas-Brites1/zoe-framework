657fc18c3c4e40e19d4c6ce76f0f619ce5597b51
Lucas Silva Brites <lucasbrites076@gmail.com>
2026-02-25 00:41:17 -0300

    fix(schema): fix Length validator checking self.min/max instead of builtin

--- source/zoe_schema/schema_validators/length.py
+++ source/zoe_schema/schema_validators/length.py
@@ -1,18 +1,17 @@
 from zoe_schema.field_schema_validator import FieldValidator
 from zoe_exceptions.schemas_exceptions.validation_exception import ValidatorException
 from typing import Any
+from numbers import Real
 
-class Length:
-    @staticmethod
-    def _min(n: int) -> None:
-       def validate(value: Any, field_name: str) -> None | ValidatorException:
-            if len(value) < n:
-                raise ValidatorException(field_name=field_name, message=f"'{field_name}' must have at least {n} characeters")
-       return validate
+class Length(FieldValidator):
+    def __init__(self: "Length", min: Real | None = None, max: Real | None = None):
+        self.min = min
+        self.max = max
 
-    @staticmethod
-    def _max(n: int) -> None:
-        def validate(value: Any, field_name: str) -> None | ValidatorException:
-            if len(value) > n:
-                raise ValidatorException(field_name=field_name, message=f"'{field_name}' exceed the maximum number of characeters from {n}")
-        return validate
+    def __call__(self: "Length", value: Any, field_name: str) -> None:
+        if self.min:
+            if len(value) < self.min:
+                raise ValidatorException(field_name=field_name, message=f"'{field_name}' must have at least {self.min} characeters")
+        if self.max:
+            if len(value) > self.max:
+                raise ValidatorException(field_name=field_name, message=f"'{field_name}' exceed the maximum number of characeters from {self.max}")
