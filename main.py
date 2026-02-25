3832bc7da87c340ba40643ee64796d8e97926e44
Lucas Silva Brites <lucasbrites076@gmail.com>
2026-02-25 00:41:41 -0300

    chore: update main with Pessoa model using NotNull, Length and Range

--- main.py
+++ main.py
@@ -7,27 +7,33 @@ from source.zoe_middlewares.limiter import Limiter
 
 from source.zoe_schema.model_schema import Model, Field
 from source.zoe_schema.schema_validators.length import Length
+from source.zoe_schema.schema_validators.not_null import NotNull
+from source.zoe_schema.schema_validators.range import Range
 
 class Pessoa(Model):
-    nome: str = Field(Length._min(n=4), Length._max(n=100))
-    idade: int
+    nome: str = Field(NotNull(), Length(max=10))
+    idade: int = Field(NotNull(), Range(min=0, max=130))
 
 class HelloHandler(Handler):
     def __call__(self: "HelloHandler", request: request.Request) -> response.Response:
+        print(request.query_params.message)
         return response.Response(http_status_code=code.HttpCode.OK)
 
 class PayloadHandler(Handler):
     def __call__(self: "PayloadHandler", request: request.Request, body: Pessoa) -> response.Response:
         print(request.path_params.user_id)
-        print(request.path_params.user_password)
         return response.Response(code.HttpCode.CREATED, body={"user": body})
 
-router: Router = Router(prefix="/teste")
+router_test: Router = Router(prefix="/teste")
 
 if __name__ == "__main__":
-    app: Zoe = Zoe(application_name="pi-api")
-    router.POST(endpoint="/user/{user_id}", handler=PayloadHandler())
-    app.use(router).use(Logger()).use(Limiter())
-    
+    app: Zoe = Zoe()
+
+    router_test\
+      .POST(endpoint="/user/{user_id}", handler=PayloadHandler())\
+      .GET(endpoint="/hello", handler=HelloHandler())
+
+    app.use(router_test).use(Logger()).use(Limiter())
+
     server: Server = Server(application=app)
-    server.run()            
+    server.run()
