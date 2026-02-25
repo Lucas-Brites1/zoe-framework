5e4de2ff7bcaf047db82c8365a0dcc5828e75308
Lucas Silva Brites <lucasbrites076@gmail.com>
2026-02-25 19:08:36 -0300

    feat: add zoe package with centralized imports for cleaner developer experience

---
+++ source/zoe/__init__.py
@@ -0,0 +1,55 @@
+# Core
+from zoe_application.application import Zoe
+from zoe_net.server import Server
+
+# HTTP
+from zoe_http.request import Request
+from zoe_http.response import Response
+from zoe_http.code import HttpCode
+from zoe_http.handler import Handler
+from zoe_http.middleware import Middleware
+
+# Router
+from zoe_router.router import Router
+from zoe_router.route import Route
+from zoe_router.routes import Routes
+
+# Schema
+from zoe_schema.model_schema import Model
+from zoe_schema.field_schema import Field
+
+# Validators
+from zoe_schema.schema_validators.not_null import NotNull
+from zoe_schema.schema_validators.length import Length
+from zoe_schema.schema_validators.range import Range
+from zoe_schema.schema_validators.email import Email
+from zoe_schema.schema_validators.pattern import Pattern
+
+# Middlewares
+from zoe_middlewares.logger import Logger
+from zoe_middlewares.limiter import Limiter
+
+# Exceptions
+from zoe_exceptions.http_exceptions.exc_http_base import ZoeHttpException
+from zoe_exceptions.http_exceptions.exc_not_found import RouteNotFoundException
+from zoe_exceptions.http_exceptions.exc_internal_exc import InternalServerException
+from zoe_exceptions.schemas_exceptions.exc_base import ErrorCode
+from zoe_exceptions.schemas_exceptions.exc_aggregate import ZoeSchemaAggregateException
+
+__all__ = [
+    # Core
+    "Zoe", "Server",
+    # HTTP
+    "Request", "Response", "HttpCode", "Handler", "Middleware",
+    # Router
+    "Router", "Route", "Routes",
+    # Schema
+    "Model", "Field",
+    # Validators
+    "NotNull", "Length", "Range", "Email", "Pattern",
+    # Middlewares
+    "Logger", "Limiter",
+    # Exceptions
+    "ZoeHttpException", "RouteNotFoundException", "InternalServerException",
+    "ErrorCode", "ZoeSchemaAggregateException",
+]
