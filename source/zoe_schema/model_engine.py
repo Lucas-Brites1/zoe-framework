0b2dc4ddaf20309b2cd5c225a8f42b3ee51b6ff4
Lucas Silva Brites <lucasbrites076@gmail.com>
2026-02-27 15:13:45 -0300

    feat: add Optional[T] and union type support in ModelEngine type validation

--- source/zoe_schema/model_engine.py
+++ source/zoe_schema/model_engine.py
@@ -2,10 +2,12 @@ from zoe_schema.model_schema import Model
 from zoe_schema.field_schema import Field
 from zoe_exceptions.schemas_exceptions.exc_base import ZoeSchemaException
 from zoe_exceptions.schemas_exceptions.exc_type import SchemaTypeError
-from zoe_exceptions.schemas_exceptions.exc_validator import SchemaValidatorException
 from zoe_exceptions.schemas_exceptions.exc_aggregate import ZoeSchemaAggregateException
+from zoe_schema.schema_validators.not_null import NotNull
+from zoe_schema.field_schema_validator import FieldValidator
 
 import typing
+import types
 
 class ModelEngine:
     @staticmethod
@@ -28,6 +30,27 @@ class ModelEngine:
         return model_class(**data)
 
     @staticmethod
+    def __is_optional(hint: type) -> bool:
+            origin = ModelEngine.__get_base_type(hint)
+            if origin is typing.Union or origin == types.UnionType:
+                args = typing.get_args(hint)
+                return type(None) in args or type(types.UnionType) in args
+            return False
+    
+    @staticmethod
+    def __contains_multiple_internal_types(hint: type) -> bool:
+            return typing.get_args(hint).__sizeof__() > 0
+
+    @staticmethod
+    def __contains_multiple_internal_types(hint: type) -> bool:
+            return typing.get_args(hint).__sizeof__() > 0
+
+    @staticmethod
+    def __get_base_type(hint: type) -> any:
+        origin = typing.get_origin(tp=hint)
+        return origin if origin is not None else hint
+
+    @staticmethod
     def __validate_model_types(hints: dict[str, type], data: dict) -> list[ZoeSchemaException]:
         type_errors: list[ZoeSchemaException] = []
 
@@ -38,7 +61,18 @@ class ModelEngine:
                 continue
 
             actual_type = type(data[field_name])
-            if actual_type != expected_type:
+            base_expected = ModelEngine.__get_base_type(hint=expected_type)
+
+            if actual_type != base_expected:
+                if ModelEngine.__is_optional(hint=typing.get_origin(expected_type)) and data[field_name] is None:
+                    continue
+                
+                print(base_expected)
+                if ModelEngine.__contains_multiple_internal_types(hint=base_expected):
+                    args: tuple = typing.get_args(tp=base_expected)
+                    if expected_type in args:
+                        continue
+
                 type_errors.append(SchemaTypeError(
                     field_name=field_name,
                     expected=expected_type,
@@ -60,12 +94,14 @@ class ModelEngine:
             if isinstance(attr_class, Field):
                 if attr_name in skip_fields:
                     continue
-
+                
                 value = data.get(attr_name, None)
 
                 for validator in attr_class.validators:
                     try:
                         if hasattr(validator, 'validate'):
+                            if not isinstance(validator, NotNull) and value is None:
+                                continue                                
                             validator.validate(value=value, field_name=attr_name)
                         else:
                             raise TypeError(
--- source/zoe_schema/model_schema.py
+++ source/zoe_schema/model_schema.py
@@ -1,9 +1,14 @@
 from typing import Any
+from typing import get_type_hints
 
 class Model:
   def __init__(self: "Model", **kwargs):
-    for key, value in kwargs.items():
-      setattr(self, key, value)
+    hints = get_type_hints(type(self))
+    for field in hints:
+      if field not in kwargs:
+        setattr(self, field, None)
+        continue
+      setattr(self, field, kwargs[field])
 
   def __getattr__(self: "Model", name: str) -> Any:
     raise AttributeError(f"'{type(self).__name__}' has no field '{name}'")
