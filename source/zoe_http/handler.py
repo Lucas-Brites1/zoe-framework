6a59df9aeda9e0c0e66a9d3bd1cf938686fe9dcb
Lucas Silva Brites <lucasbrites076@gmail.com>
2026-03-03 17:11:42 -0300

    fix: remove **kwargs from Handler protocol signature

--- source/zoe_http/handler.py
+++ source/zoe_http/handler.py
@@ -5,7 +5,7 @@ from zoe_exceptions.http_exceptions.exc_internal_exc import InternalServerExcept
 
 @runtime_checkable
 class Handler(Protocol):
-    def handle(self, request: Request, **kwargs) -> Response:
+    def handle(self, request: Request) -> Response:
         raise InternalServerException(
                 detail=f"Handler '{type(self).__name__}' must implement the handle() method"
             )
