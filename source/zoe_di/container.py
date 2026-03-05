32d8379b98a0d1f589247016ddc7d0fdaaf13fcd
Lucas Silva Brites <lucasbritesbrites076@gmail.com>
2026-03-04 21:54:16 -0300

    feat: add Container with provide, resolve and has support

--- source/zoe_di/container.py
+++ source/zoe_di/container.py
@@ -1,24 +1,34 @@
 from zoe_di.box import Box
+from zoe_di.lifecycle import PROVIDED
+from zoe_di.inspector import Inspector, ObjectKind
 from zoe_exceptions.http_exceptions.exc_internal_exc import InternalServerException
-
+from typing import Any
 class Container:
     __registry: dict[str, Box] = {}
 
     @classmethod
     def provide(cls, box: Box) -> None:
-        cls.__registry[box.key] = box.instance
+        registry_key: str = box.key or box.object_name
+        cls.__registry[registry_key] = box
 
     @classmethod
-    def provide_many(cls, *boxes: Box) -> None:
-        for box in boxes:
-            cls.provide(box)
+    def provide_instance(cls, obj: Any, key: str | None = None) -> None:
+        box = Box(obj=obj, lifecycle=PROVIDED, key=key)
+        cls.provide(box=box)
 
     @classmethod
-    def has(cls, key: str) -> bool:
-        return key in cls.__registry
+    def has(cls, key: str | Any) -> bool:
+        key_kind = Inspector.object_kind(obj=key)
+        match key_kind:
+            case ObjectKind.PRIMITIVE:
+                return key in cls.__registry
+            case ObjectKind.CLASS:
+                return key.__name__ in cls.__registry # type: ignore
+            case _:
+                return key.__class__.__name__ in cls.__registry
 
     @classmethod
-    def resolve(cls, key: str) -> Box:
+    def resolve(cls, key: str | Any) -> Box:
         if not cls.has(key):
             raise InternalServerException(
                 detail=f"No instance registered for '{key}'. "
