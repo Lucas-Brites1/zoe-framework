0465f461a4242017aa46962c7e195b5915400c6c
Lucas Silva Brites <lucasbrites076@gmail.com>
2026-03-08 15:32:12 -0300

    feat: add save method to UploadFile with dir creation and conflict resolution

--- source/zoe_http/_request_util/upload_file.py
+++ source/zoe_http/_request_util/upload_file.py
@@ -1,3 +1,6 @@
+from zoe_http._file_util import FileUtil
+from zoe_http._file_util import ROOT, Path
+import uuid
 class UploadFile:
   def __init__(self: "UploadFile", filename: str, type: str, data_bytes: bytes) -> None:
     self.__filename = filename
@@ -13,7 +16,7 @@ class UploadFile:
     return self.__data_bytes
 
   @property
-  def data(self: "UploadFile") -> str:
+  def text(self: "UploadFile") -> str:
     return self.__data_bytes.decode(encoding="utf-8")
 
   @property
@@ -23,3 +26,26 @@ class UploadFile:
   @property
   def file_type(self: "UploadFile") -> str:
     return self.__type
+
+  def save(self, path: str, froom_root: bool, filename: str | None = None, create_dirs: bool = False) -> Path | None:
+    filename_to_save = filename or self.filename
+    directory_path: Path = Path(path)
+    if froom_root:
+      directory_path = ROOT / directory_path
+
+    if not directory_path.exists():
+      if create_dirs:
+        FileUtil.create_directory(directory_path)
+      else:
+        return None
+
+    full_path: Path
+    if (directory_path / filename_to_save).exists():
+      p = Path(filename_to_save)
+      filename_to_save = f"{p.stem}_{uuid.uuid4()}{p.suffix}"
+
+    full_path = directory_path / filename_to_save
+
+    commited: bool = FileUtil.write(path=full_path, bytes_=self.data_bytes)
+
+    return full_path if commited else None
