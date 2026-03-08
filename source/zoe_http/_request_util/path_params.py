231266a526165f62e8ec828e61f5b5b8c7787e54
Lucas Silva Brites <lucasbritesbrites076@gmail.com>
2026-03-08 04:26:10 -0300

    feat: rewrite QueryParams and PathParams with typed get, multi-value support and cast with default

--- source/zoe_http/_request_util/path_params.py
+++ source/zoe_http/_request_util/path_params.py
@@ -1,8 +1,31 @@
 from typing import Any
 
-class PathParams(dict):
-    def __getattr__(self: "PathParams", key: str) -> str:
-        return self.get(key) #type: ignore
+class PathParams:
+    def __init__(self):
+        self.__pparms: dict[str, list[str]] = {}
 
-    def __setattr__(self, __name: str, __value: Any) -> None:
-        return super().__setattr__(__name, __value)
+    def _set_param(self, key: str, value: str) -> None:
+      if key in self.__pparms:
+          self.__pparms[key].append(value)
+          return
+
+      self.__pparms[key] = [value]
+
+    def __cast(self, value: str, t_cast: type, default: Any) -> Any:
+        try:
+            return t_cast(value)
+        except (ValueError, TypeError):
+            return default
+
+    def get(self, key: str, type_: type = str, default: Any | None = None) -> Any | None:
+      if key in self.__pparms:
+        value: list[Any] | Any = self.__pparms[key]
+        if len(value) > 1:
+          if type_ != str:
+            return [self.__cast(v, type_, default) for v in value]
+          return value
+        else:
+          if type_ != str:
+            return self.__cast(value=value[0], t_cast=type_, default=default)
+          return value[0]
+      return None
--- source/zoe_http/_request_util/query_params.py
+++ source/zoe_http/_request_util/query_params.py
@@ -1,8 +1,32 @@
 from typing import Any
 
-class QueryParams(dict):
-    def __getattr__(self: "QueryParams", key: str) -> str:
-        return self.get(key) #type: ignore
+class QueryParams:
+    def __init__(self):
+      self.__qparams: dict[str, list[str]] = {}
 
-    def __setattr__(self, __name: str, __value: Any) -> None:
-        return super().__setattr__(__name, __value)
+    def _set_param(self, key: str, value: str) -> None:
+      if key in self.__qparams:
+          self.__qparams[key].append(value)
+          return
+
+      self.__qparams[key] = [value]
+
+    def __cast(self, value: str, t_cast: type, default_return: Any) -> Any:
+      try:
+         return t_cast(value)
+      except (ValueError, TypeError):
+         return default_return
+
+    def get(self, key: str, type_: type = str, default: Any = None) -> list | Any | None:
+        if key in self.__qparams:
+            value: list[Any] | Any = self.__qparams[key]
+            if len(value) > 1:
+                if type_ != str:
+                  return [self.__cast(v, type_, default) for v in value]
+                return value
+            else:
+                if type_ != str:
+                    return self.__cast(value=value[0], t_cast=type_, default_return=default)
+                return value[0]
+        return None
+
