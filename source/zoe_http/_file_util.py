494cc664885e1fe7bac13a4958c2fa1a1592db02
Lucas Silva Brites <lucasbritesbrites076@gmail.com>
2026-03-08 15:32:07 -0300

    feat: add resolve_path helper to FileUtil

--- source/zoe_http/_file_util.py
+++ source/zoe_http/_file_util.py
@@ -6,6 +6,10 @@ class FileUtil:
       return str(Path(__file__).parent.parent.parent)
 
     @staticmethod
+    def create_directory(directory: Path) -> None:
+      directory.mkdir(parents=True, exist_ok=True)
+
+    @staticmethod
     def find(filename: str, directory: str | None = None) -> Path | None:
         if directory:
             full_path = (Path(directory) / filename).resolve()
@@ -25,9 +29,25 @@ class FileUtil:
                 current = parent
 
     @staticmethod
+    def resolve_path(directory: str, filename: str) -> Path:
+        return Path(directory) / filename
+
+
+    @staticmethod
+    def write(path: Path, bytes_: bytes) -> bool:
+        try:
+            with open(path, "wb") as f:
+                f.write(bytes_)
+            return True
+        except Exception as e:
+            return False
+
+    @staticmethod
     def read(path: Path) -> bytes | None:
         try:
             with open(path, "rb") as f:
                 return f.read()
         except FileNotFoundError:
             return None
+
+ROOT = FileUtil.root_path()
