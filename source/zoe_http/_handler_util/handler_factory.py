9136989d3e5cecc55d121051778bafb5e74dd481
Lucas Silva Brites <lucasbritesbrites076@gmail.com>
2026-03-08 04:26:02 -0300

    fix: resolve kwargs now raises HandlerAbortException instead of returning Response

--- source/zoe_http/_handler_util/handler_factory.py
+++ source/zoe_http/_handler_util/handler_factory.py
@@ -1,9 +1,12 @@
 from inspect import Signature, signature
 from zoe_http.handler import Handler
 from zoe_http._handler_util.handler_validator import HandlerValidator
+from zoe_exceptions.exc_non_http_internal_error import ZoeNonHttpError
+from zoe_exceptions.exc_internal_exc import InternalServerException
 from typing import Callable, Any
 from zoe_http.request import Request
 from zoe_http.response import Response
+import sys
 
 class GenericHandlerFactory:
     __prefix_generic_handler: str = "Zoe_factory_generated_handler_"
@@ -14,16 +17,15 @@ class GenericHandlerFactory:
 
     @staticmethod
     def new(fn: Callable) -> Handler:
-        HandlerValidator.validate_signature(func=fn)
-        # 1. Validate the signature of the received function that will be wrapped inside the generated generic handler
-        # 2. Generate Handler class with fixed prefix + random suffix to avoid conflicts between generated handlers
-        # 3. Internally implement the Handler protocol signature: handle(self, request: Request, ...) -> Response
-        # 4. When implementing the signature, also need to spread the kwargs that the dev's function may have
-        # 5. Internally within the generated handle function, call the dev's function passing the expected parameters and return it
-        # 6. Finally return the generated handler to the router
+        try:
+            HandlerValidator.validate_signature(func=fn)
+        except ZoeNonHttpError as e:
+            InternalServerException.from_non_http_error(error=e, request=None)
+            sys.exit(1)
+
         handler_class_name: str = GenericHandlerFactory.__generate_handler_generic_name(fn=fn)
-        sig: Signature = signature(obj=fn) # signature of the handle function declared by the dev
-        params = list(sig.parameters) # parameters from the function signature
+        sig: Signature = signature(obj=fn)
+        params = list(sig.parameters)
 
         def handle(self, request: Request, **kwargs: Any) -> Response:
             filtered_kwargs = {
@@ -33,11 +35,9 @@ class GenericHandlerFactory:
             return fn(request, **filtered_kwargs)
 
         handle.__annotations__ = fn.__annotations__
-        handler_generated: Handler = type( # type: ignore
+        handler_generated: Handler = type(  # type: ignore
             handler_class_name,
-            (Handler, ),
-            {
-                'handle': handle
-            }
+            (Handler,),
+            {'handle': handle}
         )
-        return handler_generated() # type: ignore
+        return handler_generated()  # type: ignore
