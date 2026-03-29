217915ae612e6c6f82a15987c17e975a1ac22000
Lucas Silva Brites <lucasbrites076@gmail.com>
2026-03-29 02:24:07 -0300

    wip(websocket): scaffold WebSocketWriter with initial send flow

--- source/zoe_net/zoe_websocket/websocket_writer.py
+++ source/zoe_net/zoe_websocket/websocket_writer.py
@@ -1,1 +1,11 @@
 # WIP
+from asyncio import StreamWriter
+
+class WebsocketWriter:
+  def __init__(self, connection_writer: StreamWriter) -> None:
+    self.conn_writer = connection_writer
+
+  async def write(self, frame: 'WebsocketFrame') -> None:
+    self.conn_writer.write(frame.bytes)
+    await self.conn_writer.drain()
+
